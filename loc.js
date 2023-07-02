f7f3e73adb5f84a8467d27deaec69eb517a67a31
阿吉 <36737369+lxhguard@users.noreply.github.com>
2023-07-02 23:17:31 +0800

    Create loc.js

---
+++ loc.js
@@ -0,0 +1,7 @@
+if (location.href.includes('http://') || location.href.includes('https://')) {
+				if (!location.href.includes('xn--kcr637g')) {
+					window.location.href = "https://xn--kcr637g.asia/";
+				}
+			}
+
+console.log('loc js completed');
